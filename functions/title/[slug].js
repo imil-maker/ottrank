b83d60f1ff87815a621eab8cee9cc3b3d3178adf
imil-maker <imil6939@gmail.com>
2026-06-01 05:05:16 +0900

    Update [slug].js

--- functions/title/[slug].js
+++ functions/title/[slug].js
@@ -1,20 +1,22 @@
-/* functions/title/[slug].js - 오뜨랑 작품 상세 페이지 라우팅  11  */
+/* functions/title/[slug].js - 오뜨랑 작품 상세 페이지 라우팅  12  */
 
 export async function onRequest(context) {
-  const { params, request, env } = context;
+  const { params, env } = context;
   const slug = params.slug;
 
-  // /_title_detail.html 을 가져와서 그대로 반환
-  // URL에 ?slug= 를 붙여서 JS에서 읽을 수 있게 함
-  const url = new URL(request.url);
-  url.pathname = '/_title_detail.html';
-  url.searchParams.set('slug', slug);
+  // /_title_detail.html 원본 HTML 가져오기
+  const response = await env.ASSETS.fetch('https://ottrank.kr/_title_detail.html');
+  let html = await response.text();
 
-  const response = await env.ASSETS.fetch(url.toString());
+  // </head> 바로 앞에 slug 값을 전역 변수로 주입
+  // title_detail.js에서 window.__TITLE_SLUG__ 로 읽음
+  html = html.replace('</head>', `<script>window.__TITLE_SLUG__="${slug}";</script>\n</head>`);
 
-  // HTML 응답을 그대로 반환하되, 브라우저 URL은 원래 /title/:slug 유지
-  return new Response(response.body, {
-    status: response.status,
-    headers: response.headers,
+  return new Response(html, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/html;charset=utf-8',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    },
   });
 }
